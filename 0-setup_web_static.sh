f21b778d9d66aa17d81015497e5c5193c48db096
Martins Ndifon <martinsndifon@gmail.com>
2023-04-09 22:30:41 +0000

    update to task 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,15 @@
 # set up the web server for the deployment of web_static
 
 apt update -y
-which nginx > testfile.txt
-file="testfile.txt"
+apt install nginx -y
+#which nginx > testfile.txt
+#file="testfile.txt"
 
-if [ ! -s "$file" ]
-then
-	apt install nginx -y
-fi
-rm $file
+#if [ ! -s "$file" ]
+#then
+#	apt install nginx -y
+#fi
+#rm $file
 
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
