48b47256ae6a61572034eee4debbfeb176d8fdfe
Martins Ndifon <martinsndifon@gmail.com>
2023-04-21 11:48:12 +0000

    update to 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,21 +6,25 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Greetings, hbnb project"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def textc(text):
     """Displays 'C' followed by the value of the text variable"""
     text = text.replace('_', ' ')
     return f"C {escape(text)}"
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,10 +33,12 @@ def textpython(text='is cool'):
     text = text.replace('_', ' ')
     return f"Python {escape(text)}"
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """displays “<n> is a number” only if n is an integer"""
     return f"{escape(n)} is a number"
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
